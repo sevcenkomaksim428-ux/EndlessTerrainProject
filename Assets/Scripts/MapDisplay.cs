c08f5feafe4ead6f506a6d7e3b93ffd5cf3af585
sevcenkomaksim428-ux <sevcenkomaksim428@gmail.com>
2026-04-14 10:48:51 +0300

    add mesh

--- Assets/Scripts/MapDisplay.cs
+++ Assets/Scripts/MapDisplay.cs
@@ -3,25 +3,11 @@ using UnityEngine;
 public class MapDisplay : MonoBehaviour
 {
     public Renderer textureRender;
+    public MeshFilter meshFilter;
+    public MeshRenderer meshRenderer;
 
-    public void DrawNoiseMap(float[,] noiseMap)
+    public void DrawTexture(Texture2D texture)
     {
-        int width = noiseMap.GetLength(0);
-        int height = noiseMap.GetLength(1);
-
-        Texture2D texture = new Texture2D(width, height);
-
-        Color[] colourMap = new Color[width * height];
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                colourMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
-            }
-        }
-
-        texture.SetPixels(colourMap);
-        texture.Apply();
 
         textureRender.sharedMaterial.SetTexture("_BaseMap", texture);
         textureRender.sharedMaterial.mainTexture = texture;
@@ -29,6 +15,12 @@ public class MapDisplay : MonoBehaviour
         textureRender.sharedMaterial.color = Color.white;
         textureRender.sharedMaterial.SetColor("_BaseColor", Color.white);
 
-        textureRender.transform.localScale = new Vector3(width, 1, height);
+        textureRender.transform.localScale = new Vector3(texture.width, 1, texture.height);
+    }
+
+    public void DrawMesh(MeshData meshData, Texture2D texture)
+    {
+        meshFilter.sharedMesh = meshData.CreateMesh();
+        meshRenderer.sharedMaterial.mainTexture = texture;
     }
 }
